9d9caaa52d7027d979ae31304c6d7bcae5710fe6
Mitchell Solomon <msolomonqwerty@gmail.com>
2021-04-05 21:08:17 -0700

    First commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, Button, MenuItem, Menu, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+
+import logo from '../../assets/navbar-rope.jpg';
+import useStyles from './styles';
+
+const Navbar = () => {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <AppBar position="fixed" className={classes.AppBar} color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title} color="inherit">
+                        <img src={logo} alt="rope" height="25px" className={classes.image} />
+                        Rope.com
+                    </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.button}>
+                    <Button variant="outlined" color="primary" href="#outlined-buttons">Report Bug</Button>
+                        <IconButton aria-label="Show cart items" color="inherit">
+                            <Badge badgeContent={2} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+
+            <AppBar position="bottom" className={classes.AppBar} color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title} color="inherit">
+                        <img src={logo} alt="rope" height="25px" className={classes.image} />
+                        Rope.com
+                    </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.button}>
+                    <Button variant="outlined" color="primary" href="#outlined-buttons">Report Bug</Button>
+                        <IconButton aria-label="Show cart items" color="inherit">
+                            <Badge badgeContent={2} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
+
+export default Navbar
