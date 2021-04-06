93848f95af8424965b8126f5ff9c729adca2c89a
Mitchell Solomon <msolomonqwerty@gmail.com>
2021-04-05 21:34:08 -0700

    Fixed Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -27,24 +27,6 @@ const Navbar = () => {
                     </div>
                 </Toolbar>
             </AppBar>
-
-            <AppBar position="bottom" className={classes.AppBar} color="inherit">
-                <Toolbar>
-                    <Typography variant="h6" className={classes.title} color="inherit">
-                        <img src={logo} alt="rope" height="25px" className={classes.image} />
-                        Rope.com
-                    </Typography>
-                    <div className={classes.grow} />
-                    <div className={classes.button}>
-                    <Button variant="outlined" color="primary" href="#outlined-buttons">Report Bug</Button>
-                        <IconButton aria-label="Show cart items" color="inherit">
-                            <Badge badgeContent={2} color="secondary">
-                                <ShoppingCart />
-                            </Badge>
-                        </IconButton>
-                    </div>
-                </Toolbar>
-            </AppBar>
         </div>
     )
 }
